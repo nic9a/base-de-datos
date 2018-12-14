0ac2519924dd10c150c406b0475a0a6aad6e7d3e
Paula Aguilera <44885648+Pd-Abel@users.noreply.github.com>
2018-12-13 22:41:07 -0300

    update
    actualicé sólo este archivo, actualizar los template para que reciban los datos que se requiere ingresar

--- app/views.py
+++ app/views.py
@@ -17,7 +17,7 @@ def index():
 	group by fecha
 	order by fecha
 	"""
-	print sql 
+	print sql
 	cur.execute(sql)
 	resultados  = cur.fetchall()
 
@@ -30,7 +30,7 @@ def pilotos():
 	from pilotos
 	order by pilotos.nombre
 	"""
-	print sql 
+	print sql
 	cur.execute(sql)
 	pilotos  = cur.fetchall()
 
@@ -44,7 +44,7 @@ def circuitos():
 	from circuitos
 	order by fecha
 	"""
-	print sql 
+	print sql
 	cur.execute(sql)
 	circuitos  = cur.fetchall()
 
@@ -57,7 +57,7 @@ def escuderias():
 	from circuitos
 	order by escuderias.nombre
 	"""
-	print sql 
+	print sql
 	cur.execute(sql)
 	escuderias  = cur.fetchall()
 
@@ -70,41 +70,101 @@ def proveedores():
 	from proveedores_marcas
 	order by nombre
 	"""
-	print sql 
+	print sql
 	cur.execute(sql)
 	Proveedores_motores  = cur.fetchall()
 
 	return render_template("proveedores.html", proveedores = proveedores)
 
-@app.route('/paises')
-def paises():
-	sql ="""
-	select id, pais, continente
-	from paises
-	order by continente
-	"""
-	print sql 
-	cur.execute(sql)
-	paises = cur.fetchall()
 
-	return render_template("paises.html", paises = paises)
-
-
-@app.route('/insertarp', methods=['GET', 'POST']) 
-def insertarp(pais):
+@app.route('/insertarp', methods=['GET', 'POST'])
+def insertarp(prov):
 	if request.method == 'POST':
 		nombre =  request.form['nombre']
-	        pais =  request.form['pais']
-		
+		pais =  request.form['pais']
+
 		pais1 = sql=""" select paises.id from paises where nombre='%s'"""%(pais)
 		cur.execute(sql)
 
-		sql = """ insert into Proveedores_motores  
-		(nombre,pais1) 
-		values ('%s','%s', ) returning rut_proveedor; """%(nombre,pais1)
+		sql = """ insert into Proveedores_motores
+		(nombre,pais_id)
+		values ('%s','%s'); """%(nombre,pais1)
 		insert = cur.execute(sql)
 		conn.commit()
 	return render_template('insertarp.html' )
+
+@app.route('/insertarescuderia', methods=['GET', 'POST'])
+def insertarp(escuderia):
+	if request.method == 'POST':
+		nombre =  request.form['nombre']
+		jefe =  request.form['jefe_motores']
+		proveedor =  request.form['proveedor']
+		pais =  request.form['pais']
+
+		pais1 = sql=""" select paises.id from paises where nombre='%s'"""%(pais)
+		cur.execute(sql)
+		prov = sql=""" select id from proveedores_marcas where nombre='%s'"""%(proveedor)
+		cur.execute(sql)
+
+		sql = """ insert into escuderias
+		(nombre,jefe_motores,proveedores_id,pais_id)
+		values ('%s','%s','%s','%s' ); """%(nombre, jefe, prov,pais1)
+		insert = cur.execute(sql)
+		conn.commit()
+	return render_template('insertarescuderia.html' )
+
+@app.route('/insertarpais', methods=['GET', 'POST'])
+def insertarp(pais):
+	if request.method == 'POST':
+		pais =  request.form['pais']
+		continente =  request.form['continente']
+
+		sql = """ insert into paises
+		(pais,continente)
+		values ('%s','%s'); """%(pais, continente)
+		insert = cur.execute(sql)
+		conn.commit()
+	return render_template('insertarpais.html' )
+
+@app.route('/insertarpilotos', methods=['GET', 'POST'])
+def insertarp(piloto):
+	if request.method == 'POST':
+		numero = request.form['numero']
+		nombre =  request.form['nombre']
+		apellido =  request.form['apellido']
+		edad =  request.form['edad']
+		pais =  request.form['pais']
+		escuderia =  request.form['escuderia']
+
+		esc = sql=""" select escuderias.id from escuderias where nombre='%s'"""%(escuderia)
+		cur.execute(sql)
+		pais1 = sql=""" select paises.id from paises where nombre='%s'"""%(pais)
+		cur.execute(sql)
+
+		sql = """ insert into pilotos
+		(numero, nombre, apellido, edad, pais, escuderia)
+		values ('%s','%s','%s','%s','%s','%s' ) returning rut_proveedor;
+		"""%(numero,nombre,apellido,edad,pais1, ecs)
+		insert = cur.execute(sql)
+		conn.commit()
+	return render_template('insertarpilotos.html' )
+
+app.route('/insertarcircuito', methods=['GET', 'POST'])
+def insertarp(circuito):
+	if request.method == 'POST':
+		nombre =  request.form['nombre']
+		pais =  request.form['pais']
+		fecha =  request.form['fecha']
+
+		pais1 = sql=""" select paises.id from paises where nombre='%s'"""%(pais)
+		cur.execute(sql)
+
+		sql = """ insert into circuitos
+		(nombre,pais_id, fecha)
+		values ('%s','%s', '%s'); """%(nombre, pais1, fecha)
+		insert = cur.execute(sql)
+		conn.commit()
+	return render_template('insertarcircuito.html' )
 
 @app.route('/eliminarPais', methods=['GET', 'POST'])
 def eliminar(id):
@@ -116,5 +176,3 @@ def eliminar(id):
 	cur.execute(sql)
 	conn.commit()
 	return  redirect(request.referrer)
-
-
