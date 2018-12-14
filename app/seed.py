211877150ad278345b67bfdc732bb8448f0f3fe9
nic9a <nicolas.ramireza@mail.udp.cl>
2018-12-13 23:20:16 -0300

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -1,41 +1,34 @@
-from configuraciones import *
+from .configuraciones import *
 import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 
 sql ="""
-insert into paises (pais,continente) values ('Inglaterra, Europa '),('Italia, Europa '),('Francia, Europa'),
-('Estados Unidos, America'),('Australia, Oceania');
+insert into paises (pais,continente) values ('Inglaterra', 'Europa'),('Italia', 'Europa '),('Francia', 'Europa'),
+('Estados Unidos', 'America'),('Australia', 'Oceania');
 """
 cur.execute(sql)
-sql =""",
-insert into pilotos (numero,nombre,apellido,edad,pais,escuderia) values ('44 ','Lewis ','Halmiton ','33','Inglaterra ','0') returning id;
+sql ="""
+insert into pilotos (numero,nombre,apellido,edad,pais,escuderia) values ('44 ','Lewis','Halmiton','33','1','0');
 """
 cur.execute(sql)
 
 sql ="""
-insert into provedores_marcas (nombre,pais_id) values ('Renault ','0 ') returning id;
+insert into proveedores_marcas (nombre,pais_id) values ('Renault','1');
 """
 cur.execute(sql)
 
 sql ="""
-insert into escuderias (nombre,jefe_motores,provedores_id,pais_id) values ('Mclaren','Pat Fry','0','0') returning id;
+insert into escuderias (nombre,jefe_motores,provedores_id,pais_id) values ('Mclaren','Pat Fry','1','1');
 """
 cur.execute(sql)
 
 sql ="""
-insert into circuitos (nombre,pais_id) values ('MELBOURNE ','0 ',now()) returning id;
+insert into circuitos (nombre,pais_id,fecha) values ('MELBOURNE','1','1000');
 """
 cur.execute(sql)
 
 sql ="""
-insert into resultados (circuito_id,piloto_id,accidente, puesto,puntacion) values ('0 ','0 ','4','35') returning id;
+insert into resultados (circuito_id,piloto_id,accidente, puesto,puntuacion) values ('1','1','1','35','35');
 """
 cur.execute(sql)
-
-
-
-
-conn.commit()
-cur.close()
-conn.close()
